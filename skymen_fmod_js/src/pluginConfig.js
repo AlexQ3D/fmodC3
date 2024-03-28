352f953a82eb7ce98bb7c3adcc7d27315c6283e7
Alex <metroid3d2@gmail.com>
2024-03-27 17:03:10 -0700

    Tiny buffer, big gainz

--- skymen_fmod_js/src/pluginConfig.js
+++ skymen_fmod_js/src/pluginConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   addonType: "plugin",
   id: "skymen_fmod_js",
   name: "(FMOD) JS API",
-  version: "1.0.0.5",
+  version: "1.0.0.6",
   category:
     // "3d",
     // "data-and-storage",
